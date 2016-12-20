c5d8895d43e0ed09179757af2fdb12c8a9db29aa
marcos69 <marcos69@users.noreply.github.com>
2016-12-20 03:36:37 +0100

    update to support the esp32 serial only.
    just added some #ifdef ESP8266

--- IODirector.h
+++ IODirector.h
@@ -26,12 +26,14 @@
 // Three supported channels for I/O
 enum CHANNELS {SERIAL_IO, FILE_IO, NET_IO};
 
+#ifdef ESP8266
 // Server port IODirector listens on
 const int SERVER_PORT = 21;
 
 // The networking components
 WiFiServer server(SERVER_PORT);
 WiFiClient serverClient;
+#endif
 
 // IODirector class definition
 class IODirector {
@@ -101,6 +103,7 @@ class IODirector {
           break;
 
         case NET_IO:
+#ifdef ESP8266
           if (! serverClient.connected()) {
             // Wait for a connection to be made
             serverClient = server.available();
@@ -119,6 +122,7 @@ class IODirector {
           for (int i = 0; i < charCount; i++) {
             serverClient.write(get(&outQueue));
           }
+#endif
           break;
       }
     }
@@ -184,6 +188,7 @@ class IODirector {
       // Save new channel
       currentChannel = channel;
 
+#ifdef ESP8266
       // Network I/O selected ?
       if (channel == NET_IO) {
         if ((! serverClient) || (! serverClient.connected())) {
@@ -193,6 +198,7 @@ class IODirector {
           }
         }
       }
+#endif
     }
 
     // See if input queue has any chars to return
